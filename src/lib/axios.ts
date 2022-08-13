f81e63f90c8f7de88206ae43576e45d9b873bf24
Jean Martins <jeanmrtns4@gmail.com>
2022-08-13 15:27:39 -0300

    refactor: api token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: import.meta.env.VITE_GITHUB_API,
   params: {
-    q: '',
-    repo: '',
+    auth: import.meta.env.VITE_GITHUB_TOKEN,
   },
 })
