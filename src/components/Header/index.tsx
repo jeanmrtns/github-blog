4ee6fb6792489fa07a71d08a1dfa284f07a8caee
Jean Martins <jeanmrtns4@gmail.com>
2022-08-13 17:14:12 -0300

    refactor: adjust background url

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import GithubBlogLogoImg from '../../assets/github-blog-logo.svg'
 
 export function Header() {
   return (
-    <header className="pb-[134px] pt-16 w-full bg-center bg-cover bg-[url('/public/img/header-cover.svg')]">
+    <header className="pb-[134px] pt-16 w-full bg-center bg-cover bg-[url('/img/header-cover.svg')]">
       <Link to="/">
         <img src={GithubBlogLogoImg} className="block mx-auto" alt="" />
       </Link>
